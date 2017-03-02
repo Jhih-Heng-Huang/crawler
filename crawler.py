da2d1cf2078a9d937a081627d399ffb41511e5d4
Jhih-Heng-Huang <kklive128@gmail.com>
2017-03-02 17:14:40 +0800

    nothing relevant in crawler.py

--- crawler.py
+++ crawler.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 # import lxml
 
-res = requests.get('http://www.mangatown.com/manga/boku_no_hero_academia/')
+mangaUrl = 'http://www.mangatown.com/manga/boku_no_hero_academia/'
+res = requests.get(mangaUrl)
 
 file = open("temp.txt","w")
 file.write(res.content)
@@ -12,16 +13,19 @@ file.close()
 # soup = BeautifulSoup(res.text,'lxml')
 soup = BeautifulSoup(res.content,'html.parser')
 
-mangaList = soup.find('ul',{'class':'chapter_list'})
-file = open("output.txt","wb")
-for line in mangaList.findAll('a'):
-	url = line['href']
-	print url
-	res = requests.get(url)
-	soup = BeautifulSoup(res.content,'html.parser')
-	picUrl = soup.find('meta',{'property':'og:image'})
-	print picUrl['content']
-	# file.write(line['href']+'\n')
-	# url = line['href']
+chapterList = soup.find('ul',{'class':'chapter_list'})
+# file = open("output.txt","wb")
+# for chapter in chapterList.findAll('a'):
+# 	url = chapter['href']
+# 	print url
+# 	res = requests.get(url)
+# 	soup = BeautifulSoup(res.content,'html.parser')
+# 	picUrl = soup.find('meta',{'property':'og:image'})
+# 	print picUrl['content']
+	# file.write(chapter['href']+'\n')
+	# url = chapter['href']
 	# res = requests.get(url)
-file.close()
+# file.close()
+
+for title in chapterList.findAll("span",{"class":""}):
+	print type(str(title.string))
