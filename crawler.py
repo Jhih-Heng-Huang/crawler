f15b057e427cdf4c8722f7090fbb232cd07194bb
AlbertHuang <kklive128@gmail.com>
2018-02-01 16:48:45 +0800

    <crawler> - refine crawler 	- add main() 	- define the function for getting all URL of each chapter from the manga reviewer: AlbertHuang

--- crawler.py
+++ crawler.py
@@ -2,30 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 # import lxml
 
-mangaUrl = 'http://www.mangatown.com/manga/boku_no_hero_academia/'
-res = requests.get(mangaUrl)
 
-file = open("temp.txt","w")
-file.write(res.content)
-file.close()
-
-
-# soup = BeautifulSoup(res.text,'lxml')
-soup = BeautifulSoup(res.content,'html.parser')
-
-chapterList = soup.find('ul',{'class':'chapter_list'})
 # file = open("output.txt","wb")
 # for chapter in chapterList.findAll('a'):
 # 	url = chapter['href']
 # 	print url
-# 	res = requests.get(url)
-# 	soup = BeautifulSoup(res.content,'html.parser')
+# 	website = requests.get(url)
+# 	soup = BeautifulSoup(website.content,'html.parser')
 # 	picUrl = soup.find('meta',{'property':'og:image'})
 # 	print picUrl['content']
 	# file.write(chapter['href']+'\n')
 	# url = chapter['href']
-	# res = requests.get(url)
+	# website = requests.get(url)
 # file.close()
 
-for title in chapterList.findAll("span",{"class":""}):
-	print type(str(title.string))
+
+def TestToGetAllChapterURL(manga_url):
+	manga_url = 'http://manga.fascans.com/manga/my-hero-academia'
+	website = requests.get(manga_url)
+
+	# output file
+	file = open("temp.html","w")
+	file.write(website.content)
+	file.close()
+
+
+	# soup = BeautifulSoup(website.text,'lxml')
+	soup = BeautifulSoup(website.content,'html.parser')
+	chapterList = soup.find('ul',{'class':'chapters'})
+
+	# get all url of every chapter of manga
+	for chapter in chapterList.findAll("h5",{"class":"chapter-title-rtl"}):
+		a = chapter.find("a")
+		print a['href']
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
