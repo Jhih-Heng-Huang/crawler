58d02736fe85509f8d091f40a53bc831e0b722cb
Jhih-Heng-Huang <kklive128@gmail.com>
2018-02-11 11:39:59 +0800

    <crawler> - rename and combine the files
    	- MangaCrawler.py --> crawler.py
    	- MangasteamCrawler.py is combined to manga_crawler.py
    	- manga_crawler.py --> MangaCrawler.py
    
    reviewer: AlbertHuang

--- MangaCrawler.py
+++
@@ -1,6 +0,0 @@
-class MangaCrawler(object):
-	def __init__(self, main_url):
-		self.main_url = main_url
-
-	def DownloadAllPages(self, chapter_url):
-		pass
--- MangasteamCrawler.py
+++
@@ -1,8 +0,0 @@
-import manga_crawler as manga
-
-class MangaStreamCrawler(manga.MangaCrawler):
-	def __init__(self, main_url):
-		super(MangaStreamCrawler, self).__init__(main_url)
-
-	def DownloadAllPages(self, chapter_url):
-		print 'manga stream url:' + chapter_url
