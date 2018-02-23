8887a2b7c5d638a765614fc5e4b6de6b50b4a52d
Jhih-Heng-Huang <kklive128@gmail.com>
2018-02-23 19:43:44 +0800

    <crawler> refine main.py and crawler.py
    	- make the class Manga change to an abstract class
    	- refine two methods in class Manga
    		- SelectChapter(self)
    		- DownloadChapter(self, chapt)
    	- finish the MangaStream object
    
    reviewer: AlbertHuang

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,26 +6,74 @@ class Manga(object):
 	def __init__(self, main_url):
 		self.main_url = main_url
 
-	def DownloadChapter(self, head_title = '', page_num = 0, url = ''):
-		pass
+	def SelectChapter(self):
+		# gen a list of the URL of the chapter
+		chapts = self.GenMangaChapts()
+		# select a chapter to download
+		for title, url in chapts:
+			index = str(chapts.index((title, url)))
+			print  index + '. ' + title
+		index = input('\n' + 'Select an index you want to download:')
+		(title, url) = chapts[index]
+		self.DownloadChapter(chapts[index])
+
+	def DownloadChapter(self, chapt):
+		(title, url) = chapt
+		page_num = self.GetPageNum(url)
+		for page in xrange(1,page_num + 1):
+			if not self.DownloadImg(chapt, page):
+				break
+
+	def GenMangaChapts(self):
+		raise NotImplementedError
+	def GenTitle(self, title):
+		raise NotImplementedError
+	def GenRealURL(self, url):
+		raise NotImplementedError
+	def GetPageNum(self, url):
+		raise NotImplementedError
+	def DownloadImg(self, chapt, page):
+		raise NotImplementedError
 
 
 class MangaStream(Manga):
 	def __init__(self, main_url):
 		super(MangaStream, self).__init__(main_url)
 
-	def DownloadChapter(self, head_title = '', page_num = 0, url = ''):
-		for page in range(1, page_num):
-			webpage = requests.get(url + '/' + str(page))
-			# get the url of image in the page
-			soup = BeautifulSoup(webpage.content, 'html.parser')
-			image = soup.find('img', id='manga-page')
-			if image is None:
-				print 'no page ' + str(page)
-				break
-			else:
-				print image['src']
-				pic_content = requests.get('http:' + image['src']).content
-				pic = open(str(head_title) + str(page).zfill(3) + '.png', 'wb')
-				pic.write(pic_content)
-				pic.close()
+	def GenMangaChapts(self):
+		webpage = requests.get(self.main_url)
+		soup = BeautifulSoup(webpage.content, 'html.parser')
+		stack = soup.find('table',
+			class_='table table-striped').find_all('a')
+		chapts = list()
+		for a in stack:
+			title = self.GenTitle(a.contents[0])
+			url = self.GenRealURL(a['href'])
+			chapts.append((title, url))
+		chapts.reverse()
+		return chapts
+
+	def DownloadImg(self, chapt, page):
+		(title, url) = chapt
+		webpage = requests.get(url + str(page))
+		# get the url of image in the page
+		soup = BeautifulSoup(webpage.content, 'html.parser')
+		image = soup.find('img', id='manga-page')
+		if image is None:
+			print 'no page ' + str(page)
+			return False
+		else:
+			print image['src']
+			pic_content = requests.get('http:' + image['src']).content
+			pic = open(title + '.' + str(page).zfill(3) + '.png', 'wb')
+			pic.write(pic_content)
+			pic.close()
+			return True
+
+	def GenTitle(self, title):
+		return re.match(r'^\d*', title, 0).group()
+	def GenRealURL(self, url):
+		return 'https://readms.net' + re.sub(r'\d$', '', url)
+	def GetPageNum(self, url):
+		return 100
+		
--- main.py
+++ main.py
@@ -2,33 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from crawler import MangaStream as MStream
 
-
-
-def TestToGetAllChapterURL(manga_url):
-	website = requests.get(manga_url)
-
-	# output file
-	file = open("chapter_list.html","w")
-	file.write(website.content)
-	file.close()
-
-
-	# get all url of every chapter of manga
-	soup = BeautifulSoup(website.content,'html.parser')
-	chapterList = soup.find('ul',{'class':'chapters'})
-	for chapter in chapterList.findAll("h5",{"class":"chapter-title-rtl"}):
-		a = chapter.find("a")
-		print a['href']
-
 def main():
-	manga_url = 'http://manga.fascans.com/manga/my-hero-academia'
-	# TestToGetAllChapterURL(manga_url)
-
-	# to test showing the URL of all pages
-	chapter_url = 'https://readms.net/r/my_hero_academia/170/4882/'
+	manga_url = 'https://readms.net/manga/my_hero_academia'
 	manga = MStream(manga_url)
-	manga.DownloadChapter(head_title = 'hero_170.',
-		page_num = 30, url = chapter_url)
+	manga.SelectChapter()
 
 
 if __name__ == '__main__':
